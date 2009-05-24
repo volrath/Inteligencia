9d390c290c41d62d1ed0f8f365d09e9ab79e6b9f
Daniel Barreto <volrath.fp@gmail.com>
2009-05-24 19:06:39 -0430

    Given that resolve a 8-tiles subproblems of an 15puzz involves 16!/8! solutions, which is a LOT, im changing that  to resolve a 6-tiles subproblem. The amount of each db expected will be: 16!/10! * 3bytes / 1024 = ~160M

--- 15-puzzle/pdb-generator.cc
+++ 15-puzzle/pdb-generator.cc
@@ -5,16 +5,27 @@
 
 using namespace std;
 
-void pdb_gen07(state15_t state, pattern_t *p) {
+int construct_initial2(char ** input, state15_t *state) {
+  int nums[NUM_TILES];
+  for (int i = ZERO; i < NUM_TILES; i++) {
+    nums[i] = atoi(input[i+1]);
+    if (nums[i] < 0 || nums[i] > 15)
+      return(0);
+  }
+  state->set_state(nums);
+  return(1);
+}
+
+void pdb_gen05(state15_t state, pattern_t *p) {
   (*p).p1_ = ZERO; (*p).p2_ = ZERO;
   for (int i = 7; i >= ZERO; --i) {
     (*p).p1_ = (*p).p1_ << 4; (*p).p2_ = (*p).p2_ << 4;
-    (*p).p1_ = (*p).p1_ + (state.cont(i) >= HALF_NUM_TILES ? 0xF : state.cont(i));
-    (*p).p2_ = (*p).p2_ + (state.cont(i+HALF_NUM_TILES) >= HALF_NUM_TILES ? 0xF : state.cont(i+HALF_NUM_TILES));
+    (*p).p1_ = (*p).p1_ + (state.cont(i) >= 0 && state.cont(i) < 5 ? state.cont(i) : 0xF);
+    (*p).p2_ = (*p).p2_ + (state.cont(i+HALF_NUM_TILES) >= 0 && state.cont(i+HALF_NUM_TILES) < 5 ? state.cont(i+HALF_NUM_TILES) : 0xF);
   }
 }
 
-void pdb_gen815(state15_t state, pattern_t *p) {
+void pdb_gen1015(state15_t state, pattern_t *p) {
   (*p).p1_ = ZERO; (*p).p2_ = ZERO;
   for (int i = 7; i >= ZERO; --i) {
     (*p).p1_ = (*p).p1_ << 4; (*p).p2_ = (*p).p2_ << 4;
@@ -23,7 +34,7 @@ void pdb_gen815(state15_t state, pattern_t *p) {
   }
 }
 
-void successors07(state15_t state, state15_t ** scs, bool * is_important) {
+void successors05(state15_t state, state15_t ** scs, bool * is_important) {
   memset(scs, ZERO, sizeof(scs));
   memset(is_important, ZERO, sizeof(bool));
 
@@ -100,21 +111,27 @@ class pt_hash_t : public __gnu_cxx::hash_map<pattern_t, int> { };  // class
 int pdb_bfs(state15_t *state, int cost, pt_hash_t *closed) {
   // Insert this state into the pattern database
   pattern_t pt;
-  pdb_gen07(*state, &pt);
-  cout << cost << " -- " << pt << endl;
+  if (cost > 22657) {
+    cout << *state << endl;
+    cout << "hola" << endl;
+    pdb_gen05(*state, &pt);
+    cout << pt << endl;
+  }
+  else
+    pdb_gen05(*state, &pt);
+  //cout << cost << " -- " << pt.p1_ << pt.p2_ << endl;
   // ...
   closed->insert(make_pair(pt, cost));
 
-  state15_t *scs07[BR];
-  bool is_important07[BR];
-  successors07(*state, scs07, is_important07);
-  for (int i = 0; scs07[i] != NULL && i < BR; i++) {
-    pdb_gen07(*scs07[i], &pt);
+  state15_t *scs05[BR];
+  bool is_important05[BR];
+  successors05(*state, scs05, is_important05);
+  for (int i = 0; scs05[i] != NULL && i < BR; i++) {
+    pdb_gen05(*scs05[i], &pt);
     if (!closed->count(pt))
-      pdb_bfs(scs07[i], cost + (is_important07[i] ? 1 : 0), closed);
+      pdb_bfs(scs05[i], cost + (is_important05[i] ? 1 : 0), closed);
+    //free(scs05[i]);
   }
-
-  //  free(scs07);
   return(0);
 }
 
@@ -123,9 +140,14 @@ int main(int argc, char **argv) {
   pt_hash_t closed;
 
   pdb_bfs(&state, 0, &closed);
+//   if (!construct_initial2(argv, &state)) { std::cout << "Error initializing" << std::endl; return(0); }
 //   pattern_t p;
-//   pdb_gen815(state, &p);
+//   pdb_gen05(state, &p);
 //   cout << p << endl;
 //   cout << state;
+//   state15_t *scs[BR]; bool im[BR];
+//   successors05(state, scs, im);
+//   for (int i = 0; scs[i] != NULL; i++)
+//     cout << *scs[i] << endl;
   return(0);
 }
