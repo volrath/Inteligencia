bd1337ced4512b9fde824ba07c9f6573fa72dd8a
Kristoffer <kristoffer@hephaestus.(none)>
2009-05-24 20:01:50 -0430

    Cambios necesarios en el generador de la BD :P ahora es iterativo

--- 15-puzzle/pdb-generator.cc
+++ 15-puzzle/pdb-generator.cc
@@ -2,15 +2,16 @@
 #include "lib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <queue>
 
 using namespace std;
 
 void pdb_gen07(state15_t state, pattern_t *p) {
   (*p).p1_ = ZERO; (*p).p2_ = ZERO;
-  for (int i = 7; i >= ZERO; --i) {
+    for (int i = 7; i >= ZERO; --i) {
     (*p).p1_ = (*p).p1_ << 4; (*p).p2_ = (*p).p2_ << 4;
-    (*p).p1_ = (*p).p1_ + (state.cont(i) >= HALF_NUM_TILES ? 0xF : state.cont(i));
-    (*p).p2_ = (*p).p2_ + (state.cont(i+HALF_NUM_TILES) >= HALF_NUM_TILES ? 0xF : state.cont(i+HALF_NUM_TILES));
+    (*p).p1_ = (*p).p1_ + (state.cont(i) >= 6 ? 0xF : state.cont(i));
+    (*p).p2_ = (*p).p2_ + (state.cont(i+HALF_NUM_TILES) >= 6 ? 0xF : state.cont(i+HALF_NUM_TILES));
   }
 }
 
@@ -23,17 +24,15 @@ void pdb_gen815(state15_t state, pattern_t *p) {
   }
 }
 
-void successors07(state15_t state, state15_t ** scs, bool * is_important) {
-  memset(scs, ZERO, sizeof(scs));
-  memset(is_important, ZERO, sizeof(bool));
+void successors07(state15_t state, state15_t  ** scs, bool * is_important) {
 
   short as = state.allowed_steps(), k = ZERO;
   for (int i = ZERO; i < BR; i++, as = as >> 1) {
     scs[i] = NULL;
     if (as & 1 == 1) {
       // Clone the current state
-      state15_t * clone = (state15_t *)malloc(sizeof(state15_t));
       bool important = false;
+      state15_t * clone = (state15_t *)malloc(sizeof(state15_t));
       clone->p1_ = state.p1_; clone->p2_ = state.p2_;
       if (i == 0) {
 	is_important[k] = (clone->cont(clone->bpos()-HSTEP) <= HALF_NUM_TILES ? true : false);
@@ -51,7 +50,7 @@ void successors07(state15_t state, state15_t ** scs, bool * is_important) {
 	is_important[k] = (clone->cont(clone->bpos()+VSTEP) <= HALF_NUM_TILES ? true : false);
 	clone->down();
       }
-      scs[k] = clone; k++;
+      scs[k] = clone;k++;
     }
   }
 }
@@ -97,35 +96,48 @@ namespace __gnu_cxx {
 };
 class pt_hash_t : public __gnu_cxx::hash_map<pattern_t, int> { };  // class
 
-int pdb_bfs(state15_t *state, int cost, pt_hash_t *closed) {
+int pdb_bfs(state15_t *state, int cost, pt_hash_t *closed, state15_t ** scs07, bool is_important07[BR]) {
   // Insert this state into the pattern database
   pattern_t pt;
   pdb_gen07(*state, &pt);
-  cout << cost << " -- " << pt << endl;
   // ...
   closed->insert(make_pair(pt, cost));
 
-  state15_t *scs07[BR];
-  bool is_important07[BR];
   successors07(*state, scs07, is_important07);
-  for (int i = 0; scs07[i] != NULL && i < BR; i++) {
+  /*  for (int i = 0; scs07[i] != NULL && i < BR; i++){
     pdb_gen07(*scs07[i], &pt);
     if (!closed->count(pt))
       pdb_bfs(scs07[i], cost + (is_important07[i] ? 1 : 0), closed);
-  }
+      }*/
 
-  //  free(scs07);
+  //free(scs07);
   return(0);
 }
 
 int main(int argc, char **argv) {
   state15_t state;
+  pair <state15_t*,int> statep;
   pt_hash_t closed;
+  pattern_t pt;
+  state15_t * scs07[BR];
+  bool is_important07[BR];
+  queue< pair<state15_t*,int> > stqueue;
+  stqueue.push(make_pair(&state,0));
 
-  pdb_bfs(&state, 0, &closed);
-//   pattern_t p;
-//   pdb_gen815(state, &p);
-//   cout << p << endl;
-//   cout << state;
+  while(stqueue.size() != 0) {
+    //cout << stqueue.size() << endl;
+    statep = stqueue.front();
+    stqueue.pop();
+    pdb_bfs(statep.first, statep.second, &closed,scs07,is_important07);
+    for(int i = 0; scs07[i] != NULL && i < BR; i++) {
+      pdb_gen07(*scs07[i], &pt);
+      if (!closed.count(pt))
+	stqueue.push(make_pair(scs07[i], statep.second + (is_important07[i] ? 1 : 0)));   
+    }
+    //   pattern_t p;
+    //   pdb_gen815(state, &p);
+    //   cout << p << endl;
+    //   cout << state;
+  }
   return(0);
 }
