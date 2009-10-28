7497ca14b2cba349cca4c0715c97ecdb50c7df19
Daniel Barreto <volrath.fp@gmail.com>
2009-10-28 17:11:44 -0430

    keyboard interruption catch

--- ANNs/neural_network.py
+++ ANNs/neural_network.py
@@ -3,7 +3,8 @@
 from math import exp
 from random import randrange
 
-from perceptron import Perceptron, plot
+from perceptron import Perceptron, plot, and_training_set, xor_training_set, \
+     or_training_set
 
 class SigmoidPerceptron(Perceptron):
     """
@@ -40,7 +41,6 @@ class NeuralNetwork(object):
         self.hidden = [SigmoidPerceptron(inputs) for i in range(hidden)]
         self.outputs = [SigmoidPerceptron(hidden) for i in range(outputs)]
 
-    # Check this!
     def get_error(self, training_set):
         """
         The addition of all the errors for all the outputs in all the examples.
@@ -61,10 +61,6 @@ class NeuralNetwork(object):
         """
         return [op.evaluate([hp.evaluate(inputs) for hp in self.hidden])
                 for op in self.outputs]
-#         results = []
-#         for op in self.outputs:
-#             results.append(op.evaluate([hp.evaluate(inputs) for hp in self.hidden]))
-#         return results
 
     def backpropagation_train(self, training_set, learning_rate=0.05):
         """
@@ -86,31 +82,37 @@ class NeuralNetwork(object):
                               for weight, inp in zip(hp.weights, hp.inputs)]
 
 def training(neural_network, training_set, learning_rate=.01,
-             max_iterations=1000):
+             max_iterations=1000, reduce_rate=False):
     """
     """
     it = 0; log = []
-    while it < max_iterations:
-        neural_network.backpropagation_train(training_set, learning_rate)
-        log.append(neural_network.get_error(training_set))
-        print 'Iteration %s - Error: %s' % (it, log[it])
-        if not log[it]:
-            break
-        it += 1
+    try:
+        while it < max_iterations or (not max_iterations):
+            neural_network.backpropagation_train(training_set, learning_rate)
+            log.append(neural_network.get_error(training_set))
+            print 'Iteration %s - Error: %s' % (it, log[it])
+            if not log[it]:
+                break
+            it += 1
+            learning_rate = learning_rate / it if reduce_rate else learning_rate
+    except KeyboardInterrupt:
+        pass
     test(neural_network, training_set)
     return log
 
 def test(neural_network, training_set):
-    total = 0; errors = 0
+    total = 0; errors = 0; zeros = 0
     for inputs, target_results in training_set:
         results = neural_network.evaluate(inputs)
         tresult = 1 if results[0] >= .5 else 0
         print 'Inputs: %s -> Result: %s [Wanted %s]' % (inputs, tresult, target_results[0])
         if target_results[0] != tresult:
             errors +=1
+        if tresult == 0:
+            zeros += 1
         total += 1
     print
-    print 'TOTAL: %s ERRORS: %s | FAILURE: %s %%' % (total, errors, errors * 100. / total)
+    print 'TOTAL: %s ERRORS: %s | FAILURE: %s %% | ZEROS: %s' % (total, errors, errors * 100. / total, zeros)
 
 def load_training_set(file_name):
     """
@@ -122,10 +124,10 @@ def load_training_set(file_name):
     training_set = []
     for line in f:
         p1, p2, area = line.split()
-        training_set.append(([float(p1), float(p2)], [int(area == 'A')]))
+        training_set.append(([float(p1), float(p2)], [int(area != 'A')]))
     f.close()
     return training_set
 
 if __name__ =='__main__':
-    plot(training(NeuralNetwork(2,5,1), load_training_set('bp_training/1000.txt'),
+    plot(training(NeuralNetwork(2,2,1), load_training_set('bp_training/1000.txt'), #[(inputs, [target_result]) for inputs, target_result in xor_training_set], #
                   learning_rate=0.01, max_iterations=6000))
