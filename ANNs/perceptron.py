7d7adbe041f2be7e3df85ddf946bf6a32b59407e
Kristoffer <kristoffer@hephaestus.(none)>
2009-11-02 15:31:53 -0430

    Perceptron actualizado

--- ANNs/perceptron.py
+++ ANNs/perceptron.py
@@ -4,7 +4,8 @@ from random import randrange
 class Perceptron(object):
     def __init__(self, inputs):
         self.inputs = inputs + 1
-        self.weights = [randrange(-5,5)/100. for i in range(0,self.inputs)] # The first one is the bias
+        #self.weights = [randrange(-5,5)/100. for i in range(0,self.inputs)] # The first one is the bias
+        self.weights = [0] * self.inputs
 
     def __repr__(self):
         return '<Weights: %s>' % self.weights
@@ -30,10 +31,22 @@ class Perceptron(object):
         Execute the training with all the examples in the training set
         """
         error = 0
-        pe = self._get_sum_error(training_set) if pre_error else None
+        #pe = self._get_sum_error(training_set) if pre_error else None
         for inputs, target_result in training_set:
             error += (target_result - self.evaluate(inputs))**2
-            self._alter_weight(inputs, target_result, learning_rate, pe)
+        if pre_error:
+            evals = []
+            for inputs, target_result in training_set:
+                evals.append(self.evaluate(inputs))
+            for i, weight in enumerate(self.weights):
+                delta = 0
+                for j,(inputs, target_result) in enumerate(training_set):
+                    inp = inputs + [1]
+                    delta += (target_result - evals[j])*inp[i]
+                self.weights[i] += learning_rate*delta
+        else:
+            for inputs, target_result in training_set:
+                self._alter_weight(inputs, target_result, learning_rate, None)
         return error / 2.
 
     def evaluate(self, inputs):
@@ -44,14 +57,14 @@ class Perceptron(object):
             raise ValueError("evaluate needs exactly %s inputs" % \
                              (self.inputs - 1))
         inp = map(int, inputs) + [1]
-        #return 1 if sum([w * x for w,x in zip(self.weights,inp)]) > 0 else -1
+        #return 1 if sum([w * x for w,x in zip(self.weights,inp)]) > 0 else 0
         return sum([w * x for w, x in zip(self.weights, inp)])
 
 class BooleanPerceptron(Perceptron):
     def _alter_weight(self, inputs, target_result, learning_rate):
         delta = target_result - self.evaluate(inputs)
         self.weights = [weight + learning_rate * delta * inp
-                        for weight, inp in zip(self.weights, [1] + inputs)]
+                        for weight, inp in zip(self.weights, inputs+[1])]
 
     def evaluate(self, inputs):
         return int(super(BooleanPerceptron, self).evaluate(inputs) > 0)
@@ -99,7 +112,8 @@ def plot(log):
     """
     import matplotlib.pyplot as plt
     plt.plot(log)
-    plt.show()
+    plt.savefig("new.png")
+    #plt.show()
 
 and_training_set = [
     ([0,0], 0),
@@ -119,6 +133,6 @@ xor_training_set = [
     ([1,0], 1),
     ([1,1], 0),
 ]
-if __name__ == '__main__':
-    plot(training(Perceptron(2), or_training_set, learning_rate=0.2, reduce_rate=False,
-                  standard_gradient_descent=False, max_iterations=100))
+if __name__ == '__main__':    
+    plot(training(Perceptron(2), and_training_set, learning_rate=0.3, reduce_rate=False,
+                  standard_gradient_descent=False, max_iterations=1000))
