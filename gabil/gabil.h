36ad76b007ecc4aa85f4de92df88eae3547a8b5f
Kristoffer <kristoffer@hephaestus.(none)>
2009-11-21 18:52:35 -0430

    Cambios para que funcione la funcion de fitness

--- gabil/gabil.h
+++ gabil/gabil.h
@@ -19,6 +19,8 @@
 #define RAND ((unsigned double) (rand() % 10000 / 10000.))
 #define LONG_RAND ((unsigned long)rand() << 32 | (unsigned long)rand())
 
+using namespace std;
+
 struct rule_t {
   unsigned long p1_, p2_;
 };
@@ -50,7 +52,7 @@ public:
  *  ALGORITHMS LIBRARY    ga_variants.cpp
  */
 // Crossovers
-void two_point_crossover(rule_t*);
+void two_point_crossover(rule_t*, rule_t*);
 // ...
 
 // Selections
--- gabil/genetics.cpp
+++ gabil/genetics.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int attributesP1[] = {4,8,4,16,5,7,14,6};
 int attributesP2[] = {5,2,4,3,4,41};
-long xorP1 = {15,4080,61440,4294901760,133143986176,17454747090944,288212783965667328
-              ,18158513697557839872};
-long xorP2 = {31,96,1920,14336,245760,576460752303161344};
+unsigned long int xorP1[] = {15,4080,61440,4294901760,133143986176,17454747090944,288212783965667328,18158513697557839872u};
+unsigned long int xorP2[] = {31,96,1920,14336,245760,576460752303161344};
 
 bool compare(hypothesis_t *a, hypothesis_t *b) {
   return a->fitness > b->fitness;
@@ -44,16 +43,16 @@ hypothesis_t* population_t::get_fittest() {
 // Creates a new random hypothesis
 hypothesis_t::hypothesis_t() {
   for (int i = 0; i < RULES_PER_DNA; i++) {
-    rule_t rule;
+    rule_t rule = rule_t();
     rule.p1_ = LONG_RAND;
     rule.p2_ = LONG_RAND;
-    rules.push_back(rule);
+    rules.push_back(&rule);
   }
 };
 
 // Creates a new hypothesis, doing crossover between the parents
 hypothesis_t::hypothesis_t(rule_t *parent1, rule_t *parent2) {
-  two_point_crossover(rules);
+  two_point_crossover(parent1, parent2);
 };
 
 // Does a simple point mutation
@@ -61,9 +60,9 @@ void hypothesis_t::mutate() {
   int rand_bit = rand() % 124;
 
   if (rand_bit < 64)
-    rules[rand() % RULES_PER_DNA].p1_ ^ (1 << rand_bit);
+    rules[rand() % RULES_PER_DNA]->p1_ ^ (1 << rand_bit);
   else
-    rules[rand() % RULES_PER_DNA].p2_ ^ (1 << rand_bit % 64);
+    rules[rand() % RULES_PER_DNA]->p2_ ^ (1 << rand_bit % 64);
 };
 
 float hypothesis_t::calc_fitness(long *training_set, int ts_size) {
@@ -73,13 +72,13 @@ float hypothesis_t::calc_fitness(long *training_set, int ts_size) {
   
   for (int i = 0; i < ts_size; i += 2) {
     for (it = rules.begin(); it < rules.end(); it++) {
-      and_p1 = it.p1_ & training_set[i];
-      and_p2 = it.p2_ & training_set[i+1];
+      and_p1 = (*it)->p1_ & training_set[i];
+      and_p2 = (*it)->p2_ & training_set[i+1];
 
       correct = true;
       for (int j = 0; j < NUM_ATTRS_P1; j++)
 	correct = correct && (and_p1 ^ xorP1[j] != 0);
-      for (int j = 0; j < NUM_ATTRS_P2; J++)
+      for (int j = 0; j < NUM_ATTRS_P2; j++)
 	correct = correct && (and_p2 ^ xorP2[j] != 0);
       corrects += (int)correct;
     }
