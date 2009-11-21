e232aabefd495c8e0ba259d474d2b4462a7eb4f6
Kristoffer <kristoffer@hephaestus.(none)>
2009-11-21 17:42:17 -0430

    Main que lee los long del archivo binario

--- gabil/main.cpp
+++ gabil/main.cpp
@@ -1,4 +1,5 @@
 #include "gabil.h"
+#include <sys/stat.h>
 #include <fstream>
 
 using namespace std;
@@ -6,24 +7,29 @@ using namespace std;
 int main(int argc, char **argv) {
   hypothesis_t *fittest;
   population_t *population;
+  struct stat results;
+  long int size;
   float best_fitness = 0.;
   int it = 0;
-
+  
   // read file, parsed if possible.
-//   ifstream::pos_type size;
-//   ifstream file("data/adult.bin", ios::in | ios::binary);
-//   size = file.tellg();
-//   char *creprs = new char[size];
+  if (stat("data/adult.bin", &results) == 0){
+    size = results.st_size/sizeof(long);
+  }
+  else{}
+    // An error occurred
+  ifstream file("data/adult.bin", ios::in | ios::binary);
+  //  size = file.tellg();
+  long * creprs = new long[size];
 
-//   file.seekg(0, ios::beg);
-//   file.read(creprs, size);
-//   file.close();
+  file.read((char*)creprs,sizeof(long)*size);
+  file.close();
 
 //   // Put it on a 'long array'
 //   unsigned long *reprs = (unsigned long *)creprs;
 //   delete [] creprs;
 
-  population = new population_t();
+/*  population = new population_t();
   while (true) { // dont know which stop condition =S
     cout << "Generando poblacion " << ++it << endl;
     fittest = population->get_fittest();
@@ -32,5 +38,5 @@ int main(int argc, char **argv) {
 
     cout << "    Fitness:     " << (float)fittest->fitness * 100 << " %%" << endl;
     cout << "    Best so far: " << (float)best_fitness * 100 << " %%" << endl;
-  }
+    }*/
 }
