e953405184174a4a9a4f23fe9dd0aef299a1e85b
Daniel Barreto <volrath.fp@gmail.com>
2009-11-23 00:23:25 -0430

    changed for testing

--- gabil/main.cpp
+++ gabil/main.cpp
@@ -12,10 +12,12 @@ int main(int argc, char **argv) {
    */
   int pop_size;
   float mutate_chance, new_children_perc;
+  bool verbose = true;
   if (argc != 1) {
     pop_size = atoi(argv[1]);
     sscanf(argv[2], "%f", &mutate_chance);
     sscanf(argv[3], "%f", &new_children_perc);
+    verbose = false;
   } else {    
     pop_size = POP_SIZE;
     mutate_chance = MUTATE_CHANCE;
@@ -24,14 +26,22 @@ int main(int argc, char **argv) {
 
   population = new population_t("data/adult.bin", pop_size, mutate_chance, new_children_perc);
   fittest = population->get_fittest(); best_so_far = fittest;
-  while (true) { // dont know which stop condition =S
-    cout << "Generando poblacion " << ++it << endl;
+  while (it < 3) {
+    it++;
+    population->next_generation();
+    if (verbose)
+      cout << "Generando poblacion " << it << endl;
     fittest = population->get_fittest();
     if (fittest->fitness > best_so_far->fitness)
     best_so_far = fittest;
-    
-    cout << "    Fitness:     " << (float)fittest->fitness * 100 << " %%" << endl;
-    cout << "    Best so far: " << (float)best_so_far->fitness * 100 << " %%" << endl;
-    population->next_generation();
+
+    if (verbose)
+      cout << "    Fitness:     " << (float)fittest->fitness * 100 << " %%" << endl;
+    else
+      cout << (float)fittest->fitness << endl;
+
+    if (verbose)
+      cout << "    Best so far: " << (float)best_so_far->fitness * 100 << " %%" << endl;
   }
+  cout << (float)best_so_far->fitness << endl;
 }
